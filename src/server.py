c5ddfacdb4c9150d12155f742172fc05e4aee68c
Benker <benkerd22@gmail.com>
2018-01-24 17:28:18 +0800

    增加了多项详细数据，基本实现数据展示

--- src/server.py
+++ src/server.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, send_from_directory
 import os
+import re
 import json
 
 template_dir = os.path.abspath('../web')
 app = Flask(__name__, static_url_path='', template_folder='f:\\dtws-crawler\\web')
+
+def get_plus_num(s):
+    ret = re.match(r'.+?\((.+?)\)', s).group(1)
+    if ret[0] == '+':
+        return ret[1:]
+    return ret
 
 def gen():
     with open('data.txt', 'r') as f:
@@ -11,29 +18,50 @@ def gen():
     
     lastest = max([x[1]['lastest'] for x in data.items()])
     header = ['状态', '性价比', '价格', '评分', '武将', '收藏人数', '昵称', '职业']
+    '状态 性价比 价格 战力 收藏人数 武将（3） 武器（6） 饰品（4） 护心镜 披风 昵称 职业'
+    ' 0     1     2    3    4      567      8-13      14-17    18    19   20  21 '
     table = []
     for sn, x in data.items():
         row = []
-        for i in range(len(header)):
+        for i in range(22):
             row.append('')
 
         if not x['exist']:
-            row[0] = 'x'
+            row[0] = '下架'
         elif x['lastest'] == lastest:
-            row[0] = 'N'
+            row[0] = '新！'
         else:
-            row[0] = ' '
+            row[0] = '  '
         
         row[1] = x['score'] // x['price']
         row[2] = x['price']
         row[3] = x['score']
+        row[4] = x['collect']
+
+        row[5], row[6], row[7] = 0, 0, 0
+        for pet in x['pets']:
+            if pet['type'] == '钻石卡':
+                if re.match(r'0/0', pet['progress']):
+                    row[5] += 1
+                elif re.match(r'\d+/10440', pet['progress']):
+                    row[6] += 1
+                elif re.match(r'\d+/1510', pet['progress']):
+                    row[7] += 1
+
+        equip = x['equip_info']
+        edict = {8:0, 9:6, 10:7, 11:8, 12:9, 13:10, 14:4, 15:5, 16:11, 17:12, 18:16}
+        for col, index in edict.items():
+            try:
+                row[col] = get_plus_num(equip[index])
+            except:
+                row[col] = ' '
+        
         try:
-            row[4] = x['pets'][0]['name']
+            row[19] = equip[15]
         except:
-            row[4] = ''
-        row[5] = x['collect']
-        row[6] = x['name']
-        row[7] = x['type']
+            row[19] = ' '
+        row[20] = x['name']
+        row[21] = x['type'].replace('（', '(').replace('）', ')')
 
         table.append(row)
     
